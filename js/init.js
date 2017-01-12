e378cfeeea2194f959a8902309beb43be50befe7
sjkurani <sjkurani@gmail.com>
2017-01-12 10:04:46 +0530

    initial commit

--- js/init.js
+++ js/init.js
@@ -1,17 +1,8 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('#sidenav-overlay').trigger( "click" );
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
 
 $(document).ready(function(){
 	$('.loader').show();
-	displaymsg();
+	//displaymsg();
+
     $('.slider').slider({interval: 3000});
     $('.scrollspy').scrollSpy(0,2400);
     $('#sidenav-overlay').trigger( "click" );
@@ -21,31 +12,19 @@ $(document).ready(function(){
     }
   );
 
-	$("#slider_div").animate({height: $(window).height() - $('#navbar').height()});
-	
-	$(window).on('resize', function(e) {
 		$("#slider_div").animate({height: $(window).height() - $('#navbar').height()});
-	});
-	//Send email after submit the form
-	$("#send_message").click(function(e){
-		var email = $('#ur_email').val();
-		var text_msg= $('#ur_message').val();
-		e.preventDefault();
-		$.ajax({
-		  type: "POST",
-		  url: 'send.php',
-		  data: { email: email, text: text_msg },
-		  success: function(data) {
-		  	$('#msg').html(data);
-		  }
+		$(window).on('resize', function(e) {
+			$("#slider_div").animate({height: $(window).height() - $('#navbar').height()});
 		});
 	});
-	});
+
 $(window).load(function() {
 	$('.loader').hide();
 });
-var i = 0;
-	function displaymsg() 
+
+
+//var i = 0;
+	/*function displaymsg() 
 	{
 		var we_are="Onemove Software Solutions|StartUp|entrepreneurs|Human Beings|Geeks|Designers";
 		var we_love="Web Design and Developement|Our Vision and Mission|Innovation and Creativity|Helping Others|Learning New Things|Designing Others Dream";
@@ -58,5 +37,5 @@ var i = 0;
 
 		if(i==5)
 		{i=-1;}	i++;
-		window.setTimeout("displaymsg()", 3500);   
-	}
+		window.setTimeout("displaymsg()", 3000);
+	}*/
